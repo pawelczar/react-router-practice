4514aa5c0391b47cdecd93349cd4c241119ad1a8
Pawel <pawel-czarnecki.o2.pl>
2022-12-29 00:03:51 +0000

    Added page for a missing route and updated Quate Detail component

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -2,13 +2,25 @@ import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_DATA = [
+	{ id: 'q1', author: 'Pawel', text: 'Learning React is fun!' },
+	{ id: 'q2', author: 'Einstain', text: 'Two things are infinite: the universe and human stupidity' },
+	{ id: 'q3', author: 'Paul', text: 'Learning front-end is awsome!' },
+];
 
 const QuoteDetails = () => {
 	const param = useParams();
+	const quote = DUMMY_DATA.find((elem) => elem.id === param.idQuote);
+
+	if (!quote) {
+		return <p>No qoute found!</p>;
+	}
+
 	return (
 		<>
-			<h1>Quote Details Page</h1>
-			<p>{param.idQuote}</p>
+			<HighlightedQuote text={quote.text} author={quote.author} />
 			<Route path={`/quotes/${param.idQuote}/comments`}>
 				<Comments />
 			</Route>
